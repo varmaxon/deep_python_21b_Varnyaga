6cf2cd243d01ba837700fa57904abce4ea356e65
varmaxon <v.varmax@yandex.ru>
2023-11-22 18:40:38 +0300

    hw3 fixed

--- 03/test_custom_list.py
+++ 03/test_custom_list.py
@@ -7,9 +7,22 @@ from custom_list import CustomList
 
 class TestCustomList(unittest.TestCase):
 
+    @staticmethod
+    def compare_lists(list_a, list_b) -> bool:
+        if len(list_a) != len(list_b):
+            return False
+
+        for i in range(len(list_a)):
+            if list_a[i] != list_b[i]:
+                return False
+
+        return True
+
     def test_new_custom_list(self):
         list_a = CustomList([1, 2, 3])
+        a_cp = list_a.copy()
         list_b = CustomList([1, 2, 3])
+        b_cp = list_a.copy()
         res = CustomList([0, 0, 0])
         with mock.patch.object(list_a,
                                '_CustomList__new_custom_list',
@@ -17,85 +30,151 @@ class TestCustomList(unittest.TestCase):
             list_a + list_b
             mock_foo.assert_called_once()
 
+        # проверим неизменность списков после операций
+        self.assertTrue(self.compare_lists(list_a, a_cp))
+        self.assertTrue(self.compare_lists(list_b, b_cp))
+
     def test_add_less_len(self):
+        list_a, list_b = CustomList([5, 1]), CustomList([1, 2, 7])
+        list_c, list_d = CustomList([1]), CustomList([2, 5])
+        a_cp, b_cp = list_a.copy(), list_b.copy()
+        c_cp, d_cp = list_c.copy(), list_d.copy()
 
-        res1 = CustomList([5, 1]) + CustomList([1, 2, 7])
-        res2 = CustomList([1]) + [2, 5]
-        res3 = [1] + CustomList([2, 5])
+        res1 = list_a + list_b
+        res2 = list_c + [2, 5]
+        res3 = [1] + list_d
 
-        self.assertEqual(str(res1), "CustomList: [6, 3, 7], sum: 16")
-        self.assertEqual(str(res2), "CustomList: [3, 5], sum: 8")
-        self.assertEqual(str(res3), "CustomList: [3, 5], sum: 8")
+        self.assertTrue(self.compare_lists(res1, [6, 3, 7]))
+        self.assertTrue(self.compare_lists(res2, [3, 5]))
+        self.assertTrue(self.compare_lists(res3, [3, 5]))
+
+        # проверим неизменность списков после операций
+        self.assertTrue(self.compare_lists(list_a, a_cp))
+        self.assertTrue(self.compare_lists(list_b, b_cp))
+        self.assertTrue(self.compare_lists(list_c, c_cp))
+        self.assertTrue(self.compare_lists(list_d, d_cp))
 
     def test_add_greater_len(self):
+        list_a, list_b = CustomList([1, 2, 7]), CustomList([5, 1])
+        list_c, list_d = CustomList([1]), CustomList([2, 5])
+        a_cp, b_cp = list_a.copy(), list_b.copy()
+        c_cp, d_cp = list_c.copy(), list_d.copy()
 
-        res1 = CustomList([1, 2, 7]) + CustomList([5, 1])
-        res2 = CustomList([2, 5]) + [1]
-        res3 = [2, 5] + CustomList([1])
+        res1 = list_a + list_b
+        res2 = list_d + [1]
+        res3 = [2, 5] + list_c
 
-        self.assertEqual(str(res1), "CustomList: [6, 3, 7], sum: 16")
-        self.assertEqual(str(res2), "CustomList: [3, 5], sum: 8")
-        self.assertEqual(str(res3), "CustomList: [3, 5], sum: 8")
+        self.assertTrue(self.compare_lists(res1, [6, 3, 7]))
+        self.assertTrue(self.compare_lists(res2, [3, 5]))
+        self.assertTrue(self.compare_lists(res3, [3, 5]))
+
+        # проверим неизменность списков после операций
+        self.assertTrue(self.compare_lists(list_a, a_cp))
+        self.assertTrue(self.compare_lists(list_b, b_cp))
+        self.assertTrue(self.compare_lists(list_c, c_cp))
+        self.assertTrue(self.compare_lists(list_d, d_cp))
 
     def test_add_equal_len(self):
+        list_a, list_b = CustomList([1, 2]), CustomList([5, 1])
+        list_c, list_d = CustomList([1, 2]), CustomList([2, 5])
+        a_cp, b_cp = list_a.copy(), list_b.copy()
+        c_cp, d_cp = list_c.copy(), list_d.copy()
 
-        res1 = CustomList([1, 2]) + CustomList([5, 1])
-        res2 = CustomList([2, 5]) + [1, 2]
-        res3 = [2, 5] + CustomList([1, 2])
+        res1 = list_a + list_b
+        res2 = list_d + [1, 2]
+        res3 = [2, 5] + list_c
 
-        self.assertEqual(str(res1), "CustomList: [6, 3], sum: 9")
-        self.assertEqual(str(res2), "CustomList: [3, 7], sum: 10")
-        self.assertEqual(str(res3), "CustomList: [3, 7], sum: 10")
+        self.assertTrue(self.compare_lists(res1, [6, 3]))
+        self.assertTrue(self.compare_lists(res2, [3, 7]))
+        self.assertTrue(self.compare_lists(res3, [3, 7]))
+
+        # проверим неизменность списков после операций
+        self.assertTrue(self.compare_lists(list_a, a_cp))
+        self.assertTrue(self.compare_lists(list_b, b_cp))
+        self.assertTrue(self.compare_lists(list_c, c_cp))
+        self.assertTrue(self.compare_lists(list_d, d_cp))
 
     def test_saved_items(self):
-        list_a = CustomList([5, 1, 3, 7])
-        list_b = CustomList([1, 2, 7])
-        list_c = [1, 2]
+        list_a, list_b, list_c = CustomList([5, 1, 3, 7]), CustomList([1, 2, 7]), [1, 2]
+        a_cp, b_cp, c_cp = list_a.copy(), list_b.copy(), list_c.copy()
 
-        res11 = list_a + list_b
-        res12 = list_c + list_a
-        res21 = list_a - list_b
-        res22 = list_c - list_b
+        res1 = list_a + list_b
+        self.assertTrue(self.compare_lists(list_a, a_cp))
+        self.assertTrue(self.compare_lists(list_b, b_cp))
 
-        self.assertEqual(str(res11), "CustomList: [6, 3, 10, 7], sum: 26")
-        self.assertEqual(str(res12), "CustomList: [6, 3, 3, 7], sum: 19")
-        self.assertEqual(str(res21), "CustomList: [4, -1, -4, 7], sum: 6")
-        self.assertEqual(str(res22), "CustomList: [0, 0, -7], sum: -7")
+        res2 = list_c + list_a
+        self.assertTrue(self.compare_lists(list_a, a_cp))
+        self.assertTrue(self.compare_lists(list_c, c_cp))
 
-        """Check that the values of the lists are saved"""
-        self.assertEqual(list_a, CustomList([5, 1, 3, 7]))
-        self.assertEqual(list_b, CustomList([1, 2, 7]))
-        self.assertEqual(list_c, [1, 2])
+        res3 = list_a - list_b
+        self.assertTrue(self.compare_lists(list_a, a_cp))
+        self.assertTrue(self.compare_lists(list_b, b_cp))
+
+        res4 = list_c - list_b
+        self.assertTrue(self.compare_lists(list_c, c_cp))
+        self.assertTrue(self.compare_lists(list_b, b_cp))
+
+        # проверка результатов операций
+        self.assertTrue(self.compare_lists(res1, [6, 3, 10, 7]))
+        self.assertTrue(self.compare_lists(res2, [6, 3, 3, 7]))
+        self.assertTrue(self.compare_lists(res3, [4, -1, -4, 7]))
+        self.assertTrue(self.compare_lists(res4, [0, 0, -7]))
 
     def test_sub_less_len(self):
+        list_a, list_b = CustomList([5, 1]), CustomList([1, 2, 7])
+        list_c, list_d = CustomList([1]), CustomList([2, 5])
+        a_cp, b_cp = list_a.copy(), list_b.copy()
+        c_cp, d_cp = list_c.copy(), list_d.copy()
 
-        res1 = CustomList([5, 1]) - CustomList([1, 2, 7])
-        res2 = CustomList([1]) - [2, 5]
-        res3 = [1] - CustomList([2, 5])
+        res1 = list_a - list_b
+        res2 = list_c - [2, 5]
+        res3 = [1] - list_d
 
-        self.assertEqual(str(res1), "CustomList: [4, -1, -7], sum: -4")
-        self.assertEqual(str(res2), "CustomList: [-1, -5], sum: -6")
-        self.assertEqual(str(res3), "CustomList: [-1, -5], sum: -6")
+        self.assertTrue(self.compare_lists(list_a, a_cp))
+        self.assertTrue(self.compare_lists(list_b, b_cp))
+        self.assertTrue(self.compare_lists(list_c, c_cp))
+        self.assertTrue(self.compare_lists(list_d, d_cp))
+
+        self.assertTrue(self.compare_lists(res1, [4, -1, -7]))
+        self.assertTrue(self.compare_lists(res2, [-1, -5]))
+        self.assertTrue(self.compare_lists(res3, [-1, -5]))
 
     def test_sub_greater_len(self):
+        list_a, list_b = CustomList([1, 2, 7]), CustomList([5, 1])
+        list_c, list_d = CustomList([1]), CustomList([2, 5])
+        a_cp, b_cp = list_a.copy(), list_b.copy()
+        c_cp, d_cp = list_c.copy(), list_d.copy()
 
-        res1 = CustomList([1, 2, 7]) - CustomList([5, 1])
-        res2 = CustomList([2, 5]) - [1]
-        res3 = [2, 5] - CustomList([1])
+        res1 = list_a - list_b
+        res2 = list_d - [1]
+        res3 = [2, 5] - list_c
 
-        self.assertEqual(str(res1), "CustomList: [-4, 1, 7], sum: 4")
-        self.assertEqual(str(res2), "CustomList: [1, 5], sum: 6")
-        self.assertEqual(str(res3), "CustomList: [1, 5], sum: 6")
+        self.assertTrue(self.compare_lists(list_a, a_cp))
+        self.assertTrue(self.compare_lists(list_b, b_cp))
+        self.assertTrue(self.compare_lists(list_c, c_cp))
+        self.assertTrue(self.compare_lists(list_d, d_cp))
+
+        self.assertTrue(self.compare_lists(res1, [-4, 1, 7]))
+        self.assertTrue(self.compare_lists(res2, [1, 5]))
+        self.assertTrue(self.compare_lists(res3, [1, 5]))
 
     def test_sub_equal_len(self):
+        list_a, list_b = CustomList([1, 2]), CustomList([5, 1])
+        list_d = CustomList([2, 5])
+        a_cp, b_cp = list_a.copy(), list_b.copy()
+        d_cp = list_d.copy()
 
-        res1 = CustomList([1, 2]) - CustomList([5, 1])
-        res2 = CustomList([2, 5]) - [1, 2]
-        res3 = [2, 5] - CustomList([1, 2])
+        res1 = list_a - list_b
+        res2 = list_d - [1, 2]
+        res3 = [2, 5] - list_a
 
-        self.assertEqual(str(res1), "CustomList: [-4, 1], sum: -3")
-        self.assertEqual(str(res2), "CustomList: [1, 3], sum: 4")
-        self.assertEqual(str(res3), "CustomList: [1, 3], sum: 4")
+        self.assertTrue(self.compare_lists(list_a, a_cp))
+        self.assertTrue(self.compare_lists(list_b, b_cp))
+        self.assertTrue(self.compare_lists(list_d, d_cp))
+
+        self.assertTrue(self.compare_lists(res1, [-4, 1]))
+        self.assertTrue(self.compare_lists(res2, [1, 3]))
+        self.assertTrue(self.compare_lists(res3, [1, 3]))
 
     @patch.object(CustomList, '__add__')
     def test_add_called(self, mock_foo):
