a8a2db7d64b6432795610fdc6bcc55e8bfb0a52c
varmaxon <v.varmax@yandex.ru>
2023-12-04 16:06:16 +0300

    hw1 fixed

--- 01/tests_model.py
+++ 01/tests_model.py
@@ -15,6 +15,9 @@ class TestModel(unittest.TestCase):
 
         predict_message_mood(msg, model)
 
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+
         self.assertTrue(mock_predict.called)
 
     @patch.object(SomeModel, 'predict')
@@ -26,6 +29,9 @@ class TestModel(unittest.TestCase):
 
         predict_message_mood(msg, model)
 
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+
         self.assertEqual(mock_predict.call_count, 1)
 
     @patch.object(SomeModel, 'predict')
@@ -36,6 +42,10 @@ class TestModel(unittest.TestCase):
         model = SomeModel()
 
         result = predict_message_mood(msg, model)
+
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+
         expected_value = "неуд"
 
         self.assertEqual(result, expected_value)
@@ -48,6 +58,10 @@ class TestModel(unittest.TestCase):
         model = SomeModel()
 
         result = predict_message_mood(msg, model)
+
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+
         expected_value = "норм"
 
         self.assertEqual(result, expected_value)
@@ -60,6 +74,10 @@ class TestModel(unittest.TestCase):
         model = SomeModel()
 
         result = predict_message_mood(msg, model)
+
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+
         expected_value = "отл"
 
         self.assertEqual(result, expected_value)
@@ -73,6 +91,9 @@ class TestModel(unittest.TestCase):
 
         result = predict_message_mood(msg, model)
 
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+
         self.assertTrue(isinstance(result, str))
 
     @patch.object(SomeModel, 'predict')
@@ -85,6 +106,8 @@ class TestModel(unittest.TestCase):
         with self.assertRaises(TypeError):
             predict_message_mood(msg, model)
 
+        self.assertFalse(mock_predict.called)
+
     @patch.object(SomeModel, 'predict')
     def test_incorrect_type_model(self, mock_predict):
         mock_predict.return_value = 0.5
@@ -94,6 +117,140 @@ class TestModel(unittest.TestCase):
 
         with self.assertRaises(TypeError):
             predict_message_mood(msg, model)
+
+        self.assertFalse(mock_predict.called)
+
+    @patch.object(SomeModel, 'predict')
+    def test_boundary_values(self, mock_predict):
+        mock_predict.return_value = 0.3
+        msg = "Вулкан"
+        model = SomeModel()
+        result = predict_message_mood(msg, model)
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+        expected_value = "норм"
+        self.assertEqual(result, expected_value)
+
+        mock_predict.return_value = 0.8
+        msg = "Вулкан"
+        model = SomeModel()
+        result = predict_message_mood(msg, model)
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+        expected_value = "норм"
+        self.assertEqual(result, expected_value)
+
+    @patch.object(SomeModel, 'predict')
+    def test_around_boundary_values(self, mock_predict):
+        mock_predict.return_value = 0.29
+        msg = "Вулкан"
+        model = SomeModel()
+        result = predict_message_mood(msg, model)
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+        expected_value = "неуд"
+        self.assertEqual(result, expected_value)
+
+        mock_predict.return_value = 0.31
+        msg = "Вулкан"
+        model = SomeModel()
+        result = predict_message_mood(msg, model)
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+        expected_value = "норм"
+        self.assertEqual(result, expected_value)
+
+        mock_predict.return_value = 0.79
+        msg = "Вулкан"
+        model = SomeModel()
+        result = predict_message_mood(msg, model)
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+        expected_value = "норм"
+        self.assertEqual(result, expected_value)
+
+        mock_predict.return_value = 0.81
+        msg = "Вулкан"
+        model = SomeModel()
+        result = predict_message_mood(msg, model)
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+        expected_value = "отл"
+        self.assertEqual(result, expected_value)
+
+    @patch.object(SomeModel, 'predict')
+    def test_change_thresholds(self, mock_predict):
+        mock_predict.return_value = 0.29
+        msg = "Вулкан"
+        model = SomeModel()
+        result = predict_message_mood(msg, model, bad_thresholds=0.2, good_thresholds=0.9)
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+        expected_value = "норм"
+        self.assertEqual(result, expected_value)
+
+        mock_predict.return_value = 0.31
+        msg = "Вулкан"
+        model = SomeModel()
+        result = predict_message_mood(msg, model, bad_thresholds=0.2, good_thresholds=0.9)
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+        expected_value = "норм"
+        self.assertEqual(result, expected_value)
+
+        mock_predict.return_value = 0.19
+        msg = "Вулкан"
+        model = SomeModel()
+        result = predict_message_mood(msg, model, bad_thresholds=0.2, good_thresholds=0.9)
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+        expected_value = "неуд"
+        self.assertEqual(result, expected_value)
+
+        mock_predict.return_value = 0.21
+        msg = "Вулкан"
+        model = SomeModel()
+        result = predict_message_mood(msg, model, bad_thresholds=0.2, good_thresholds=0.9)
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+        expected_value = "норм"
+        self.assertEqual(result, expected_value)
+
+        mock_predict.return_value = 0.79
+        msg = "Вулкан"
+        model = SomeModel()
+        result = predict_message_mood(msg, model, bad_thresholds=0.2, good_thresholds=0.9)
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+        expected_value = "норм"
+        self.assertEqual(result, expected_value)
+
+        mock_predict.return_value = 0.81
+        msg = "Вулкан"
+        model = SomeModel()
+        result = predict_message_mood(msg, model, bad_thresholds=0.2, good_thresholds=0.9)
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+        expected_value = "норм"
+        self.assertEqual(result, expected_value)
+
+        mock_predict.return_value = 0.89
+        msg = "Вулкан"
+        model = SomeModel()
+        result = predict_message_mood(msg, model, bad_thresholds=0.2, good_thresholds=0.9)
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+        expected_value = "норм"
+        self.assertEqual(result, expected_value)
+
+        mock_predict.return_value = 0.91
+        msg = "Вулкан"
+        model = SomeModel()
+        result = predict_message_mood(msg, model, bad_thresholds=0.2, good_thresholds=0.9)
+        args = mock_predict.call_args.args
+        self.assertEqual(args, ('Вулкан',))
+        expected_value = "отл"
+        self.assertEqual(result, expected_value)
 
 
 if __name__ == "__main__":
